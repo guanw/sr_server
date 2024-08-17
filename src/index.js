2866e9025582b0bc45bccfb588a13a4ebd42e939
guanw <guanw0826@gmail.com>
2024-08-16 20:58:48 -0400

    init socket io server to get ready for multi-player game

---
+++ src/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server);
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, Socket.IO with TypeScript!</h1>');
+});
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
